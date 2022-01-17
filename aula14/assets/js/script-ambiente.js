bee46f9ee93e9865a11d961f9497f0193651bca4
Moises Teixeira <moidesteixeira1312@gmail.com>
2022-01-17 01:55:55 -0300

    Aula 14 – Repetições (Parte 2)

---
+++ aula14/assets/js/script-ambiente.js
@@ -0,0 +1,18 @@
+// var i = 1;
+
+// while (i <= 5) {
+// 	console.log(i);
+// 	i++;
+// }
+
+// do {
+// 	console.log(i);
+// 	i++;
+// }	while (i <= 5);
+
+console.log("Vai começar...");
+
+for (let i = 1; i <= 5; i++)
+	console.log(i);
+
+console.log("FIM!");
